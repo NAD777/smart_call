6660c8d4b850dcdb29c93f88b03c416580a1773b
NAD777 <nehaev07.07.2003@gmail.com>
2018-07-21 15:10:49 +0300

    all done

--- main/main.ino
+++ main/main.ino
@@ -1,13 +1,18 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h> 
+#include <iarduino_RTC.h> 
+#include <string.h>  
 LiquidCrystal_I2C lcd(0x27,20,2);
+iarduino_RTC time(RTC_DS1302, 10, 13, 12);    
 String *time_change;
 int btgr=5;
-int btbl=4;
+int btbl=6;
 int btye=3;
 int btwh=2;
+int Relay = 4;
 int j=0;
 int size=0;
+int time1=0;
 int inden(int n)
 {
   int ans=0;
@@ -32,20 +37,108 @@ void setup()
   pinMode(btbl,INPUT);
   pinMode(btye,INPUT);
   pinMode(btwh,INPUT);
-  String str = "Mac one love!";
+  pinMode(Relay, OUTPUT);   
   lcd.begin();
+  time.begin();
+  time.settime(0,0,0,0,0,0,0);    // Записываем время в модуль: 0 сек, 51 мин, 21 час, 27, октября, 2015 года, вторник.
   lcd.backlight();
   Serial.begin(9600);
-  screen(0,0,str);
+  
   set();
   for(int i=0;i<size;i++)
   {
     Serial.print(time_change[i]);
     Serial.print("\n");
-  } 
+    
+  }
+  
 }
 void set()
 {
+  lcd.clear();
+  screen(0,0,"Calibrate RTC?");
+  screen(0,1,"No(bl)");
+  screen(9,1,"Yes(gr)");
+  bool do_calibrate = true;
+  while(true){
+    if(click(btbl))
+    {
+      do_calibrate = false;
+      break;
+    }
+    if(click(btgr))
+    {
+      do_calibrate = true;
+      break;
+    }
+  }
+  Serial.print(do_calibrate);
+  delay(1000);
+  if(do_calibrate == true)
+  {
+    String s="00:00";
+    lcd.clear();
+    screen(0,0,"Calibrating");
+    screen(0,1,s);
+    int ch10=0,ch1=0,min10=0,min1=0;
+    while(!click(btgr))
+    {
+      if(click(btwh))
+      {
+        min1++;
+          if(min1>=10)
+          {
+            min1-=10;
+            min10++;
+          }
+          if(min10>=6)
+          {
+            min10=0;
+            min1=0; 
+          }
+        s[4]=char(min1+48);
+        s[3]=char(min10+48);
+        lcd.clear();
+        screen(0,0,"Calibrating");
+        screen(0,1,s);
+        delay(150);
+      }
+      
+      if(click(btye))
+      {
+        ch1++;
+          if(ch1>=10)
+          {
+            ch1-=10;
+            ch10++;
+          }
+          if(ch10*10+ch1>=24)
+          {
+            ch10=0;
+            ch1=0; 
+          }
+        s[1]=char(ch1+48);
+        s[0]=char(ch10+48);
+        lcd.clear();
+        screen(0,0,"Calibrating");
+        screen(0,1,s);
+        delay(150);
+      }
+      if(click(btbl))
+      {
+        ch10=0;
+        ch1=0;
+        min10=0;
+        min1=0;
+        s="00:00";
+        lcd.clear();
+        screen(0,0,"Calibrating");
+        screen(0,1,s);
+      }
+    }
+    time.settime(0,min10*10+min1,ch10*10+ch1,0,0,0,0);    // Записываем время в модуль:  сек,  мин,  час, , октября, 2015 года, вторник.
+  }
+  delay(1000);
   int n=0;
   lcd.clear();
   screen(0,0,"Number of changes");
@@ -60,14 +153,35 @@ void set()
         screen(0,1,n);
         delay(100);
       }
+      if(click(btbl))
+      {
+        n=0;
+        lcd.clear();
+        screen(0,0,"Number of changes");
+        screen(0,1,n);
+        delay(100);
+      }
   }
+  if(n==0)
+  {
+    lcd.clear();
+    screen(0,0,"Nothing enter");
+    delay(100);
+    while(!click(btgr))
+    {
+    
+    }
+  }
+  else
+  {
   size=n*2;
   time_change=new String[n*2]; 
   delay(300);
+  
    lcd.clear();
   // screen(0,0,1);
-   screen(2,0,"time");
-   screen(0,1,"00:00-00:00");
+   //screen(2,0,"time");
+  // screen(0,1,"00:00-00:00");
    
    for(int i=0;i<n;i++)
    {
@@ -192,13 +306,66 @@ void set()
     j++;
     delay(300);
    }
-
+  }
 }
   
+bool check_time(String s)
+{
+  char str[5];
+  strcpy(str,time.gettime("H:i"));
+  //Serial.print(str); 
+  if(str[0] == s[0]&&str[1] == s[1]&&str[3] == s[3]&&str[4] == s[4])
+    return true;
+  return false;                                                           
+}
 
 void loop()
 {
-  
+  if(millis()%1000==0){
+    //char str[5];
+   // strcpy(str,time.gettime("H:i"));                                   
+      //Serial.println(time.gettime("H:i:s"));
+      lcd.clear();
+      screen(0,0,"Time:");
+      screen(6,0,time.gettime("H:i:s"));
+      screen(0,1,"Call:");
+      screen(6,1,time_change[time1]);
+      if(check_time(time_change[time1]))
+      {
+         digitalWrite(Relay, HIGH);   // реле включено
+         delay(3000);               
+         digitalWrite(Relay, LOW);  // реле выключено
+         time1++;    
+         lcd.clear();
+        screen(0,0,"Time:");
+        screen(6,0,time.gettime("H:i:s"));
+        screen(0,1,"Call:");
+        screen(6,1,time_change[time1]);                                        
+      }
+    }
+  if(click(btbl))
+  {
+    set();
+    delay(200);
+  }
+  if(click(btwh))
+  {
+    time1--;    
+    lcd.clear();
+    screen(0,0,"Time:");
+    screen(6,0,time.gettime("H:i:s"));
+    screen(0,1,"Call:");
+    screen(6,1,time_change[time1]);
+  }
+  if(click(btye))
+  {
+    time1++;    
+    lcd.clear();
+    screen(0,0,"Time:");
+    screen(6,0,time.gettime("H:i:s"));
+    screen(0,1,"Call:");
+    screen(6,1,time_change[time1]);
+  }
 }
 
 void screen(int x, int y, String s)
